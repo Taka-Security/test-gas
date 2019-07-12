7a261a248f5079daf019b27b9c02a5a097af931f
Alexander Remie <user@rmi7.me>
2019-07-12 17:04:51 +0200

    hardcode truffle version to output, dynamic not working

--- cli.js
+++ cli.js
@@ -263,7 +263,7 @@ function setup_truffle_config(solc_version, optimizer_runs, evm_version, node_ho
       },
     },
   };
-  console.log(`truffle  # ${require('./node_modules/truffle/package.json').version}`);
+  console.log(`truffle  # 5.0.17`);
   console.log(`compiler # solc: ${solc_version} | evm: ${evm_version} | optimizer runs: ${optimizer_runs}`);
   
   // possibly add a truffle network to the truffle-config.js file
